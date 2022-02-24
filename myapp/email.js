a41c9d2aeb7507626b19a76687ce334aec418c48
Alvin <97275939+xhfkd00@users.noreply.github.com>
2022-02-24 18:47:43 +0900

    동기화#2 (#5)
    * 22.02.09
    
    * 리뷰_적용
    
    * 리뷰_적용
    
    * 회원탈퇴_추가
    
    * 회원수정_추가
    
    * 회원정보수정_추가
    
    * 주석제거
    
    * 22.02.09
    
    * 리뷰_적용
    
    * 리뷰_적용
    
    * 회원탈퇴_추가
    
    * 회원수정_추가
    
    * 회원정보수정_추가
    
    * 주석제거
    
    * 세션,쿠키로로그인,로그아웃적용
    
    * 게시판추가
    
    * 라우팅,댓글api완료
    
    * generator삭제
    
    * 파일삭제 동기화
    
    * 파일삭제동기화
    
    * 동기화

---
+++ myapp/email.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    post: 587,
+    auth: {
+        user: process.env.nodemailerEmail,
+        pass: process.env.nodemailerPassword
+    }
+});
+
+module.exports = transporter;
---
+++ myapp/models/database.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql');
+
+// //mysql 연결
+const connection = mysql.createConnection({
+    host: process.env.databaseHost,
+    port: process.env.databasePort,
+    user: process.env.databaseUser,
+    password: process.env.databasePassword,
+    database: process.env.databaseName,
+    dateStrings: 'date'
+});
+
+connection.connect();
+
+module.exports = connection;
